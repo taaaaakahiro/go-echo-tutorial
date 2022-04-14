ab24e090ed4b366f46b3be5243c6b5566c065b2f
fiks <fiks@fiksnoMacBook-puro.local>
2022-04-14 10:22:28 +0900

    同じ階層にmain()関数を複数置けない

--- server.go
+++ server.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
 	"math/rand"
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	fmt.Println("hello world", rand.Intn(10))
 
-
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "Hello3333, World!")
 	})
 	// e.POST("/users", saveUser)
 	e.GET("/users/:id", getUser) //path parameter
@@ -21,19 +21,18 @@ func main() {
 	// e.PUT("/users/:id", updateUser)
 	// e.DELETE("/users/:id", deleteUser)
 
-	
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
 func getUser(c echo.Context) error {
 	// User ID from path `users/:id`
 	id := c.Param("id")
-return c.String(http.StatusOK, id)
+	return c.String(http.StatusOK, id)
 }
 
 func show(c echo.Context) error {
 	// Get team and member from the query string
 	team := c.QueryParam("team")
 	member := c.QueryParam("member")
-	return c.String(http.StatusOK, "team:" + team + ", member:" + member)
+	return c.String(http.StatusOK, "team:"+team+", member:"+member)
 }
